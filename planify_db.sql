fc2e552dd1373a1687d9c355e35f64ed952041e0
Diya Talavanekar <diorausethegolden@gmail.com>
2026-03-02 10:38:39 +0530

    you msg

--- planify_db.sql
+++ planify_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 09, 2026 at 07:07 AM
+-- Generation Time: Mar 02, 2026 at 06:07 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -38,7 +38,39 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`admin_id`, `username`, `password`) VALUES
-(1, 'Rasika Prakshale', 'Rasika123');
+(1, 'Rasika Prakshale', 'Rasika123'),
+(4, 'Diya Talavanekar', '12345678'),
+(5, 'Shital Dongare', 'Shital06'),
+(6, 'kranti', 'kranti123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bookings`
+--
+
+CREATE TABLE `bookings` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `event_id` int(11) NOT NULL,
+  `package_name` varchar(50) DEFAULT NULL,
+  `package_price` decimal(10,2) DEFAULT NULL,
+  `guests` int(11) DEFAULT NULL,
+  `veg_qty` int(11) DEFAULT NULL,
+  `nonveg_qty` int(11) DEFAULT NULL,
+  `total_amount` decimal(10,2) DEFAULT NULL,
+  `advance_amount` decimal(10,2) DEFAULT NULL,
+  `remaining_amount` decimal(10,2) DEFAULT NULL,
+  `status` varchar(20) DEFAULT 'Pending',
+  `booking_date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `bookings`
+--
+
+INSERT INTO `bookings` (`id`, `user_id`, `event_id`, `package_name`, `package_price`, `guests`, `veg_qty`, `nonveg_qty`, `total_amount`, `advance_amount`, `remaining_amount`, `status`, `booking_date`) VALUES
+(1, 1, 1, 'Basic Package', 200000.00, 1000, 200, 800, 660000.00, 132000.00, 528000.00, 'Pending', '2026-02-27 15:14:00');
 
 -- --------------------------------------------------------
 
@@ -63,13 +95,104 @@ INSERT INTO `contact_info` (`id`, `email`, `phone`, `address`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `events`
+--
+
+CREATE TABLE `events` (
+  `id` int(11) NOT NULL,
+  `event_name` varchar(100) NOT NULL,
+  `description` text NOT NULL,
+  `image` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `events`
+--
+
+INSERT INTO `events` (`id`, `event_name`, `description`, `image`) VALUES
+(1, 'Wedding Event', 'Book the hall for wedding event', '1772030300_wedding.jpg'),
+(2, 'Birthday Event', 'Book the hall for birthday parties', '1772031104_birthday.jpg'),
+(3, 'Engagement Event', 'Book the hall for engagement event.', '1772031282_engagement.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `food_items`
+--
+
+CREATE TABLE `food_items` (
+  `id` int(11) NOT NULL,
+  `type` enum('Veg','NonVeg') NOT NULL,
+  `item_name` varchar(100) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `food_items`
+--
+
+INSERT INTO `food_items` (`id`, `type`, `item_name`, `price`) VALUES
+(2, 'Veg', 'Roti', 25.00),
+(3, 'Veg', 'Chapati', 20.00);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `food_prices`
+--
+
+CREATE TABLE `food_prices` (
+  `id` int(11) NOT NULL,
+  `veg_price` decimal(10,2) NOT NULL,
+  `nonveg_price` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `food_prices`
+--
+
+INSERT INTO `food_prices` (`id`, `veg_price`, `nonveg_price`) VALUES
+(1, 300.00, 500.00);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `packages`
+--
+
+CREATE TABLE `packages` (
+  `id` int(11) NOT NULL,
+  `event_id` int(11) NOT NULL,
+  `package_name` varchar(50) NOT NULL,
+  `description` text DEFAULT NULL,
+  `package_price` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `packages`
+--
+
+INSERT INTO `packages` (`id`, `event_id`, `package_name`, `description`, `package_price`) VALUES
+(1, 1, 'Basic Package', NULL, 200000.00),
+(2, 1, 'Premium Package', NULL, 500000.00),
+(3, 1, 'Luxury Package', NULL, 700000.00),
+(4, 2, 'Basic Package', NULL, 40000.00),
+(5, 2, 'Premium Package', NULL, 50000.00),
+(6, 2, 'Luxury Package', NULL, 70000.00),
+(7, 3, 'Basic Package', NULL, 30000.00),
+(8, 3, 'Premium Package', NULL, 50000.00);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `email` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `phone` varchar(15) NOT NULL,
   `password` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -78,9 +201,8 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `password`, `created_at`) VALUES
-(1, 'diyat', 'diyatalavanekar@gmail.com', '12345678', '2026-01-24 06:30:31'),
-(2, 'shitald', 'shitaldongare@gmail.com', '098765', '2026-01-24 06:38:15');
+INSERT INTO `users` (`id`, `username`, `email`, `phone`, `password`, `created_at`) VALUES
+(1, 'diyat', 'diyatalavanekar@gmail.com', '7684934958', '12345678', '2026-02-14 15:00:46');
 
 --
 -- Indexes for dumped tables
@@ -93,17 +215,47 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`admin_id`);
 
 --
+-- Indexes for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `contact_info`
 --
 ALTER TABLE `contact_info`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `food_items`
+--
+ALTER TABLE `food_items`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `food_prices`
+--
+ALTER TABLE `food_prices`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `packages`
+--
+ALTER TABLE `packages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `event_id` (`event_id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`),
   ADD UNIQUE KEY `email` (`email`);
 
 --
@@ -114,7 +266,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `bookings`
+--
+ALTER TABLE `bookings`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `contact_info`
@@ -123,10 +281,44 @@ ALTER TABLE `contact_info`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `events`
+--
+ALTER TABLE `events`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `food_items`
+--
+ALTER TABLE `food_items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `food_prices`
+--
+ALTER TABLE `food_prices`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `packages`
+--
+ALTER TABLE `packages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `packages`
+--
+ALTER TABLE `packages`
+  ADD CONSTRAINT `packages_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
